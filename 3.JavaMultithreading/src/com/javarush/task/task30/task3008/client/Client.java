9c009f4f4f92dbe662845c6950d85852fd2a12c3
barabaska <borozdenets@gmail.com>
2017-07-18 00:27:47 +0700

    LVL 26

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -0,0 +1,110 @@
+package com.javarush.task.task30.task3008.client;
+
+import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * Created by barabaska on 17.07.17.
+ */
+public class Client {
+
+    protected Connection connection;
+    private volatile boolean clientConnected = false;
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
+
+    public class SocketThread extends Thread {
+
+        protected void processIncomingMessage(String message) {
+            ConsoleHelper.writeMessage(message);
+        }
+
+        protected void informAboutAddingNewUser(String userName) {
+            ConsoleHelper.writeMessage(userName + " присоединен к чату.");
+        }
+
+        protected void informAboutDeletingNewUser(String userName) {
+            ConsoleHelper.writeMessage(userName + " покинул чат.");
+        }
+
+        protected void notifyConnectionStatusChanged(boolean clientConnected) {
+            Client.this.clientConnected = clientConnected;
+            synchronized (Client.this) {
+                Client.this.notify();
+            }
+        }
+    }
+
+    public void run() {
+        SocketThread socketThread = getSocketThread();
+        socketThread.setDaemon(true);
+        socketThread.start();
+
+        synchronized (this) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                ConsoleHelper.writeMessage("Ошибка. Программа закрыта.");
+                System.exit(1);
+            }
+        }
+        
+        if (clientConnected)
+            ConsoleHelper.writeMessage("Соединение установлено. Для выхода наберите команду 'exit'.");
+        else ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+
+        String text = "";
+        while (clientConnected) {
+            text = ConsoleHelper.readString();
+
+            if (text.equals("exit")) {
+                clientConnected = false;
+                break;
+            }
+
+            if (shouldSendTextFromConsole()) {
+                sendTextMessage(text);
+            }
+        }
+    }
+
+    protected String getServerAddress() {
+        ConsoleHelper.writeMessage("IP адрес:");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort() {
+        ConsoleHelper.writeMessage("Порт:");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName() {
+        ConsoleHelper.writeMessage("Имя пользователя:");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Ошибка. Соединение разорвано.");
+            clientConnected = false;
+        }
+    }
+}
